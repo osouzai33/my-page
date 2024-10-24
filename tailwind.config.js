69a4d30e0c2886d2042095a11856de7eaf8d352f
souta <souta.y61233@gmail.com>
2024-10-25 00:43:15 +0900

    💄 Moving Component
    アニメーションの設定をしました

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,23 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        slide: {
+          "0%": { transform: "translateX(150%)" },
+          "100%": { transform: "translateX(-150%)" },
+        },
+        rotateFade: {
+          "0%": { transform: "rotate(0deg)", opacity: "1" },
+          "25%": { transform: "rotate(90deg)", opacity: "0" },
+          "50%": { transform: "rotate(180deg)", opacity: "1" },
+          "75%": { transform: "rotate(270deg)", opacity: "0" },
+          "100%": { transform: "rotate(360deg)", opacity: "1" },
+        },
+      },
+      animation: {
+        slide: "slide 8s linear infinite",
+        rotateFade: "rotateFade 3s ease-in-out infinite",
+      },
     },
   },
   plugins: [],
